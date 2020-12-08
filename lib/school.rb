924011ddd48f0db672d9cfddee885e0feae3a10e
Robert Gu <robertgu84@gmail.com>
2020-12-08 09:45:03 -0700

    Remove the t plz

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,9 @@
 class School
-  attr_reader :start_time 
+  attr_reader :start_time
               :hours_in_day
 
   def initialize(start_time, hours_in_day)
-    @start_time = start_time
+    @start_time = star_time
     @hours_in_day = hours_in_day
   end
 end
